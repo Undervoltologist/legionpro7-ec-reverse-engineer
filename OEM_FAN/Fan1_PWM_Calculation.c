57f83b9084ef856930a009666dfe69f9e0c5f8ee
Ubeyde Bulat <93976452+Undervoltologist@users.noreply.github.com>
2026-04-20 15:23:12 +0300

    Update and rename Fan0_PWM_Calculation.c to Fan1_PWM_Calculation.c

--- OEM_FAN/Fan1_PWM_Calculation.c
+++ OEM_FAN/Fan1_PWM_Calculation.c
@@ -1,5 +1,5 @@
 
-void Fan0_PWM_Calculation(undefined4 param_1,uint param_2)
+void Fan1_PWM_Calculation(undefined4 param_1,uint param_2)
 
 {
   uint uVar1;
@@ -7,25 +7,25 @@ void Fan0_PWM_Calculation(undefined4 param_1,uint param_2)
   int iVar3;
   
   uVar1 = 0;
-  while (uVar2 = uVar1 & 0xff, uVar2 < DAT_00101f9f) {
+  while (uVar2 = uVar1 & 0xff, uVar2 < Fan_Step_Size) {
     iVar3 = uVar1 * 0x3c;
     uVar1 = uVar1 + 1;
     if ((ushort)((ushort)**(byte **)(iVar3 + 0x24eb4) * 100) <= Fan1_RPM_HI_FanPage) {
       param_2 = uVar2;
     }
   }
-  if (Fan0RPM_Target != 0) {
-    iVar3 = (uint)Fan0RPM_Target * 100;
+  if (Fan1RPM_Target != 0) {
+    iVar3 = (uint)Fan1RPM_Target * 100;
     if ((int)(uint)Fan1_RPM_HI_FanPage < (int)(iVar3 - (uint)Static_78_Value)) {
       if (Max_PWM <= Fan1_PWM) {
         return;
       }
-      if (Fan0_Update_Timer == '\0') {
-        Fan0_Update_Timer = **(undefined **)(param_2 * 0x3c + 0x24ec4);
+      if (Fan1_Update_Timer == '\0') {
+        Fan1_Update_Timer = **(undefined **)(param_2 * 0x3c + 0x24ec4);
         Fan1_PWM = Fan1_PWM + 1;
         return;
       }
-      Fan0_Update_Timer = Fan0_Update_Timer + -1;
+      Fan1_Update_Timer = Fan1_Update_Timer + -1;
       return;
     }
     if ((uint)Fan1_RPM_HI_FanPage <= (uint)Static_78_Value + iVar3) {
@@ -33,11 +33,11 @@ void Fan0_PWM_Calculation(undefined4 param_1,uint param_2)
     }
   }
   if (Fan1_PWM != 0) {
-    if (Fan0_Update_Timer != '\0') {
-      Fan0_Update_Timer = Fan0_Update_Timer + -1;
+    if (Fan1_Update_Timer != '\0') {
+      Fan1_Update_Timer = Fan1_Update_Timer + -1;
       return;
     }
-    Fan0_Update_Timer = **(char **)(param_2 * 0x3c + 0x24ec8);
+    Fan1_Update_Timer = **(char **)(param_2 * 0x3c + 0x24ec8);
     Fan1_PWM = Fan1_PWM - 1;
   }
   return;
