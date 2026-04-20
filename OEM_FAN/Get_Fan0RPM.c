8bff50f9dcdfe7642d97558f1d0a058189fbff17
Ubeyde Bulat <93976452+Undervoltologist@users.noreply.github.com>
2026-04-20 15:32:32 +0300

    Update Get_Fan0RPM.c

--- OEM_FAN/Get_Fan0RPM.c
+++ OEM_FAN/Get_Fan0RPM.c
@@ -16,18 +16,18 @@ void Get_Fan0RPM(void)
   }
   else if ((uVar2 <= (uint)DAT_00101f0f * 100) || (iVar3 = 1, (uint)DAT_00101f1f * 100 <= uVar2))
   goto LAB_000491a4;
-  Static_78_Value = **(byte **)(iVar3 * 0x10 + 0x251c8);
+  Fan0_Tolerance = **(byte **)(iVar3 * 0x10 + 0x251c8);
 LAB_000491a4:
-  if (Static_78_Value == 0) {
-    Static_78_Value = 0x32;
+  if (Fan0_Tolerance == 0) {
+    Fan0_Tolerance = 0x32;
   }
-  MainEC_Fan0CurrentRPM = (char)((uVar1 & 0xffff) / 100);
-  if (100 < uVar2 % 100 + (uint)Static_78_Value) {
-    MainEC_Fan0CurrentRPM = MainEC_Fan0CurrentRPM + '\x01';
+  MainEC_Fan0CurrentRPM*100 = (char)((uVar1 & 0xffff) / 100);
+  if (100 < uVar2 % 100 + (uint)Fan0_Tolerance) {
+    MainEC_Fan0CurrentRPM*100 = MainEC_Fan0CurrentRPM*100 + '\x01';
   }
   Fan0_RPM_HI_FanPage = (short)uVar1;
   Fan0_PWM_Target = Fan0_PWM;
-  Fan0_CurrentRPM*100 = MainEC_Fan0CurrentRPM;
+  Fan0_CurrentRPM*100 = MainEC_Fan0CurrentRPM*100;
   return;
 }
 
