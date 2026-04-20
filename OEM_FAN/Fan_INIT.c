d715a3d1172437c05f23e43a5ad88aa13ad728eb
Ubeyde Bulat <93976452+Undervoltologist@users.noreply.github.com>
2026-04-20 18:37:03 +0300

    Update Fan_INIT.c

--- OEM_FAN/Fan_INIT.c
+++ OEM_FAN/Fan_INIT.c
@@ -8,29 +8,29 @@ void Fan_INIT(void)
   Get_Fan1RPM();
   Get_Fan0RPM();
   Get_Fan2RPM();
-  bVar1 = DAT_00101ff6 >> 1 & 1;
-  if ((DAT_00101ff6 >> 1 & 1) == 0) {
-    iVar2 = Check_System_isNotSleep();
+  bVar1 = FanTableToggle >> 1 & 1;
+  if ((FanTableToggle >> 1 & 1) == 0) {
+    iVar2 = Check_System_Status();
     if (iVar2 != 0) {
       Fan0_PWM = bVar1;
       Fan1_PWM = bVar1;
       Fan2_PWM = bVar1;
       return;
     }
-    iVar2 = Check_FanTable();
+    iVar2 = Check_CustomMode_MaxRPM();
     if (iVar2 != 0) {
       Fan0_PWM = Max_PWM;
       Fan1_PWM = Max_PWM;
       Fan2_PWM = Max_PWM;
       return;
     }
-    if (DAT_00100c06 == '\0') {
+    if (ErrorFlag? == '\0') {
       Fan1_FailSafe();
       Fan0_Failsafe();
       Fan2_Failsafe();
       return;
     }
-    DAT_00100c06 = DAT_00100c06 + -1;
+    ErrorFlag? = ErrorFlag? + -1;
   }
   Fan1_PWM = FUN_0004a37a(0x20,Max_PWM);
   Fan0_PWM = FUN_0004a37a(0x20,Max_PWM);
