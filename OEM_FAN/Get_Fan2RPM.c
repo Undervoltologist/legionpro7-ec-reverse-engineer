1ad4391978e9a21c461407513e5c48e12584bd41
Ubeyde Bulat <93976452+Undervoltologist@users.noreply.github.com>
2026-04-20 15:42:09 +0300

    Update Get_Fan2RPM.c

--- OEM_FAN/Get_Fan2RPM.c
+++ OEM_FAN/Get_Fan2RPM.c
@@ -11,18 +11,19 @@ void Get_Fan2RPM(void)
     uVar1 = 0x20e6da / (uVar1 + (uint)Fan2_TACH_LO * 0x100);
   }
   uVar2 = uVar1 & 0xffff;
-  if (((uint)DAT_00101f0e * 100 < uVar2) && (uVar2 < (uint)DAT_00101f1e * 100)) {
+  if (((uint)TACH_HI_LOW_SPEED_BOUNDARY * 100 < uVar2) &&
+     (uVar2 < (uint)TACH_HI_HIGH_SPEED_BOUNDARY * 100)) {
     iVar3 = 0;
   }
-  else if ((uVar2 <= (uint)DAT_00101f0f * 100) || (iVar3 = 1, (uint)DAT_00101f1f * 100 <= uVar2))
-  goto LAB_00049428;
-  Static_78_Value = **(byte **)(iVar3 * 0x10 + 0x251c8);
+  else if ((uVar2 <= (uint)TACH_LO_LOW_SPEED_BOUNDARY * 100) ||
+          (iVar3 = 1, (uint)TACH_LO_HIGH_SPEED_BOUNDARY * 100 <= uVar2)) goto LAB_00049428;
+  Fan2_Tolerance = **(byte **)(iVar3 * 0x10 + 0x251c8);
 LAB_00049428:
-  if (Static_78_Value == 0) {
-    Static_78_Value = 0x32;
+  if (Fan2_Tolerance == 0) {
+    Fan2_Tolerance = 0x32;
   }
   MainEC_Fan2_CurrentRPM*100 = (char)((uVar1 & 0xffff) / 100);
-  if (100 < uVar2 % 100 + (uint)Static_78_Value) {
+  if (100 < uVar2 % 100 + (uint)Fan2_Tolerance) {
     MainEC_Fan2_CurrentRPM*100 = MainEC_Fan2_CurrentRPM*100 + '\x01';
   }
   Fan2_RPM_HI = (short)uVar1;
