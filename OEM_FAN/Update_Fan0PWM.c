10b12b40d89bef4f7efe61f72a102a976281fe5e
Ubeyde Bulat <93976452+Undervoltologist@users.noreply.github.com>
2026-04-20 14:56:53 +0300

    Create Update_Fan0PWM.c

---
+++ OEM_FAN/Update_Fan0PWM.c
@@ -0,0 +1,47 @@
+
+void Update_Fan0PWM(void)
+
+{
+  bool bVar1;
+  int iVar2;
+  
+  if ((Fan_Control_Toggle & 2) == 0) {
+    if ((Fan_Control_Toggle >> 5 & 1) == 0) {
+      return;
+    }
+    if (Max_PWM < Fan0_PWM) {
+      Fan0_PWM = Max_PWM;
+      return;
+    }
+    if (Fan0_PWM <= Fan0_PWM_Override) {
+      bVar1 = Fan0_PWM < Fan0_PWM_Override;
+Increase/Decrease_Fan0_PWM:
+      if (!bVar1) {
+        return;
+      }
+      if (Max_PWM <= Fan0_PWM) {
+        return;
+      }
+      Fan0_PWM = Fan0_PWM + 1;
+      return;
+    }
+  }
+  else {
+    if (Max_PWM < Fan0_PWM) {
+      Fan0_PWM = Max_PWM;
+      return;
+    }
+    if (Fan0_RPM_Override != 0) {
+      iVar2 = (uint)Fan0_RPM_Override * 100;
+      if ((uint)Fan0_RPM_HI <= iVar2 + (uint)Static_78_Value) {
+        bVar1 = (int)(uint)Fan0_RPM_HI < (int)(iVar2 - (uint)Static_78_Value);
+        goto Increase/Decrease_Fan0_PWM;
+      }
+    }
+  }
+  if (Fan0_PWM != 0) {
+    Fan0_PWM = Fan0_PWM - 1;
+  }
+  return;
+}
+
